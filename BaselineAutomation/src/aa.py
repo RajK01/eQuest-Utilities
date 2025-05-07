d72e3173629610dad9d63bdb63aa28c495e80ed5
Rajeev Kumar <97374811+RajK01@users.noreply.github.com>
2025-05-07 15:07:47 +0530

    Update aa.py

--- BaselineAutomation/src/aa.py
+++ BaselineAutomation/src/aa.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def zoneSpace(file_path):
     # Step 1: Read the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         inp_data = file.readlines()
 
     # Step 2: Identify the markers for the "HVAC Systems / Zones" section
